70410ec3643fe24b6317076dcc14e25323c436a5
Vagrant Default User <vagrant@stretch.localdomain>
2017-10-04 11:05:15 +0000

    add 2 entities

--- src/GameBundle/Entity/Comment.php
+++ src/GameBundle/Entity/Comment.php
@@ -1,25 +1,59 @@
 <?php
+
 namespace GameBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Comment
+ *
+ * @ORM\Table(name="comment")
+ * @ORM\Entity(repositoryClass="GameBundle\Repository\CommentRepository")
+ */
 class Comment
 {
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     private $id;
-    private $gameId;
-    private $userId;
-    private $note;
-    private $comment;
-
-    function __construct(int $id, int $gameId, int $userId, int $note, string $comment)
-    {
-        $this->id = $id;
-        $this->gameId = $gameId;
-        $this->userId = $userId;
-        $this->note = $note;
-        $this->comment = $comment;
-    }
 
     /**
-     * @return mixed
+     * @var int
+     *
+     * @ORM\Column(name="gameId", type="integer")
+     */
+    private $gameId;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="userId", type="integer")
+     */
+    private $userId;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="note", type="integer")
+     */
+    private $note;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="comment", type="string", length=255)
+     */
+    private $comment;
+
+
+    /**
+     * Get id
+     *
+     * @return int
      */
     public function getId()
     {
@@ -27,7 +61,23 @@ class Comment
     }
 
     /**
-     * @return mixed
+     * Set gameId
+     *
+     * @param integer $gameId
+     *
+     * @return Comment
+     */
+    public function setGameId($gameId)
+    {
+        $this->gameId = $gameId;
+
+        return $this;
+    }
+
+    /**
+     * Get gameId
+     *
+     * @return int
      */
     public function getGameId()
     {
@@ -35,7 +85,23 @@ class Comment
     }
 
     /**
-     * @return mixed
+     * Set userId
+     *
+     * @param integer $userId
+     *
+     * @return Comment
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return int
      */
     public function getUserId()
     {
@@ -43,7 +109,23 @@ class Comment
     }
 
     /**
-     * @return mixed
+     * Set note
+     *
+     * @param integer $note
+     *
+     * @return Comment
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return int
      */
     public function getNote()
     {
@@ -51,11 +133,27 @@ class Comment
     }
 
     /**
-     * @return mixed
+     * Set comment
+     *
+     * @param string $comment
+     *
+     * @return Comment
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string
      */
     public function getComment()
     {
         return $this->comment;
     }
-
 }
+
--- src/GameBundle/Entity/User.php
+++ src/GameBundle/Entity/User.php
@@ -1,33 +1,59 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: probe
- * Date: 26/09/2017
- * Time: 22:06
- */
-
 
 namespace GameBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="GameBundle\Repository\UserRepository")
+ */
 class User
 {
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     private $id;
-    private $mail;
-    private $nickname;
-    private $lastname;
-    private $password;
-
-    function __construct(int $id, string $mail, string $nickname, string $lastname, string $password)
-    {
-        $this->id = $id;
-        $this->mail = $mail;
-        $this->nickname = $nickname;
-        $this->lastname = $lastname;
-        $this->password = $password;
-    }
 
     /**
-     * @return mixed
+     * @var string
+     *
+     * @ORM\Column(name="mail", type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nickname", type="string", length=255)
+     */
+    private $nickname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastname", type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+
+    /**
+     * Get id
+     *
+     * @return int
      */
     public function getId()
     {
@@ -35,7 +61,23 @@ class User
     }
 
     /**
-     * @return mixed
+     * Set mail
+     *
+     * @param string $mail
+     *
+     * @return User
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string
      */
     public function getMail()
     {
@@ -43,7 +85,23 @@ class User
     }
 
     /**
-     * @return mixed
+     * Set nickname
+     *
+     * @param string $nickname
+     *
+     * @return User
+     */
+    public function setNickname($nickname)
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    /**
+     * Get nickname
+     *
+     * @return string
      */
     public function getNickname()
     {
@@ -51,7 +109,23 @@ class User
     }
 
     /**
-     * @return mixed
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string
      */
     public function getLastname()
     {
@@ -59,12 +133,27 @@ class User
     }
 
     /**
-     * @return mixed
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
      */
     public function getPassword()
     {
         return $this->password;
     }
-
-
 }
+
