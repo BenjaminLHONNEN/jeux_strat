d95b97b283b31a97d9fe7a196c67381a210ebcfc
Vagrant Default User <vagrant@stretch.localdomain>
2017-10-09 10:48:51 +0000

    changemant formulaire

--- src/GameBundle/Controller/UserController.php
+++ src/GameBundle/Controller/UserController.php
@@ -8,6 +8,10 @@
 
 namespace GameBundle\Controller;
 
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use GameBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -18,11 +22,38 @@ class UserController extends Controller
      * @Route("/user/sign/{typeOfSign}", name="sign")
      */
     public function signAction(string $typeOfSign){
+        $user = new User();
+
         if ($typeOfSign !== "up") {
             $typeOfSign = "in";
+        } else {
+            $form = $this->createFormBuilder($user)
+                ->add('pseudo', TextType::class)
+                ->add('password', TextType::class)
+                ->add('mail', TextType::class)
+                ->add('save', SubmitType::class, array('label' => 'Send Comment'))
+                ->getForm();
         }
-            return $this->render('GameBundle:Game:sign.html.twig', [
-                "typeOfSign" => $typeOfSign,
-            ]);
+
+
+
+        $form->handleRequest($request);
+        $gameRepository = $this->getDoctrine()->getRepository("GameBundle\Entity\Game");
+        $game = $gameRepository->find($gameId);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+
+            $comment->setGame($game);
+            $comment->setUserId("1");
+
+            $em->persist($comment);
+            $em->flush();
+            $isCommentSave = true;
+        }
+        return $this->render('GameBundle:Game:sign.html.twig', [
+            "typeOfSign" => $typeOfSign,
+        ]);
     }
 }
